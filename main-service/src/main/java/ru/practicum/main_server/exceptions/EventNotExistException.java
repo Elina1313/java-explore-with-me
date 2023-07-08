8915e8d318c5eadafd798fada8d9f240c45280a3
Elina1313 <112032648+Elina1313@users.noreply.github.com>
2023-07-08 13:52:56 +0300

    feat: step 2

--- main-service/src/main/java/ru/practicum/main_server/exceptions/EventNotExistException.java
+++ main-service/src/main/java/ru/practicum/main_server/exceptions/EventNotExistException.java
@@ -1,7 +1,7 @@
 package ru.practicum.main_server.exceptions;
 
-public class EventNotExistException extends RuntimeException{
-    public EventNotExistException(String message){
-            super(message);
-        }
+public class EventNotExistException extends RuntimeException {
+    public EventNotExistException(String message) {
+        super(message);
+    }
 }
